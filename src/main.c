aca8ef197e2ef26fc8b4305afa4b6ba791ca79e7
Tai Zeming <tai.zeming@pithos.net>
2025-07-06 21:46:57 +0800

    feat: init SDL window

--- src/main.c
+++ src/main.c
@@ -1,6 +1,37 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_video.h>
+#include <stdbool.h>
 #include <stdio.h>
 
-int main() {
-  printf("Hello, world!\n");
+bool is_running = false;
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
+bool initialize_window(void) {
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0) {
+    fprintf(stderr, "Error initializing SDL.\n");
+    return false;
+  }
+
+  window = SDL_CreateWindow("Island", 800, 600,
+                            SDL_WINDOW_RESIZABLE | SDL_WINDOW_BORDERLESS);
+  if (!window) {
+    fprintf(stderr, "Error creating SDL window.\n");
+    return false;
+  }
+
+  renderer = SDL_CreateRenderer(window, NULL);
+  if (!renderer) {
+    fprintf(stderr, "Error creating SDL renderer.\n");
+    return false;
+  }
+
+  return true;
+}
+
+int main(void) {
+  /* Create a SDL window */
+  is_running = initialize_window();
+
   return 0;
 }
